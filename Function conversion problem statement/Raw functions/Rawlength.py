e6887b190e39006d58416a30d10daef3459e03c1
Sasijon <jvs.gps@gmail.com>
2022-08-06 19:27:50 +0530

    Add files via upload

---
+++ Function conversion problem statement/Raw functions/Rawlength.py
@@ -0,0 +1,12 @@
+def RawLength(s):
+    #length is present in Azure data factory
+    return length(s)
+    
+    
+        
+if __name__ == "__main__":
+    s = 'A'
+    
+    op = RawLength(s)
+    print (op)
+        
