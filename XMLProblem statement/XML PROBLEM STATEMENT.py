c7dbf97d4847fc9ebee1488c7d5cec11cbd20ce2
Sasijon <jvs.gps@gmail.com>
2022-08-06 23:44:34 +0530

    Add files via upload

--- XMLProblem statement/XML PROBLEM STATEMENT.py
+++ XMLProblem statement/XML PROBLEM STATEMENT.py
@@ -1,11 +1,44 @@
-import fileinput
+import xml.etree.ElementTree as ET
 
-def xml_to_linegae_conversion(s):
-    pass
-    return 1
+tree = ET.parse('C:\Users\Latha\OneDrive\Desktop\wf_src_idw_cntry_multi_def_cd.xml')
+
+root = tree.getroot()
+
+source1 = root[0][1][0].attrib
+source2 = root[0][1][1].attrib
+source3 = root[0][1][2].attrib
+
+target1 = root[0][1][3].attrib
+target2 = root[0][1][4].attrib
 
 
-s = fileinput.file()
-for x in xml_to_linegae_conversion(s):
-    print(x)
+mapping1 = root[0][0][0].attrib
+
+shortcut_s3 = root[0][0][1].attrib
+shortcut_t2 = root[0][0][2].attrib
+config_1 = root[0][0][3].attrib
+
+session_1 = root[0][0][4].attrib
+
+mapping_1 = root[0][0][5].attrib
+shortcut_s2_dummy = root[0][0][6].attrib
+
+shortcut_s1 = root[0][0][7].attrib
+
+shortcut_t1 = root[0][0][8].attrib
+
+config_2 = root[0][0][9].attrib
+
+session_2 = root[0][0][10].attrib
+
+workflow = root[0][0][11].attrib
+
+transformations1 = root[0][0][0][0].attrib
+
+
+
+transformations2 = root[0][0][0][1].attrib
+
+
+print(root[0].attrib)
 
